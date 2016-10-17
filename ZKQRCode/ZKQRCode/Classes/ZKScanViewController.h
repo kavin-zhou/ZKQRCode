02491ed111039bfde8eac6a8ec30f21f066f951e
ZK <dev_zk@163.com>
2016-10-17 17:34:06 +0800

    sync

---
+++ ZKQRCode/ZKQRCode/Classes/ZKScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZKScanViewController.h
+//  ZKQRCode
+//
+//  Created by ZK on 16/10/17.
+//  Copyright © 2016年 ZK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZKScanViewController : UIViewController
+
+@end
